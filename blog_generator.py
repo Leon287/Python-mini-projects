cb01c888d3b5a6d6dee5e28f45ee928acc562f3e
Leon287 <dhruvjp04@gmail.com>
2025-05-03 09:46:38 +0530

    Create blog_generator.py
    Added a small description on top of the program

---
+++ blog_generator.py
@@ -0,0 +1,28 @@
+# A simple blog generator using llama3
+import requests
+
+def generate_blog(paragraph_topic):
+    prompt = f"Write a paragraph about the following topic: {paragraph_topic}"
+
+    response = requests.post(
+        'http://localhost:11434/api/generate',
+        json={
+            "model": "llama3",
+            "prompt": prompt,
+            "stream": False 
+        }
+    )
+    data = response.json()
+    return data['response']
+
+print(generate_blog('Why meditation is important?'))
+
+keep_writing = True
+
+while keep_writing:
+    answer = input('Write a paragraph? Y for yes, anything else for no: ')
+    if answer.upper() == 'Y':
+        paragraph_topic = input('What should this paragraph talk about? ')
+        print(generate_blog(paragraph_topic))
+    else:
+        keep_writing = False
