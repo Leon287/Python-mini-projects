cf366b73434224868b84dabb62c8683f7d585424
Leon287 <dhruvjp04@gmail.com>
2025-05-04 10:32:51 +0530

    Update Gifcreator.py

--- Gifcreator.py
+++ Gifcreator.py
@@ -1,9 +1,9 @@
 import imageio.v3 as iio 
 
-filenames = ['images\\image1.jpg','images\\image2.jpg','images\\image3.jpg']
+filenames = ['images\\image1.png','images\\image2.png','images\\image3.png']
 images = []
 
 for filename in filenames:
     images.append(iio.imread(filename))
 
-iio.imwrite('wojak-meme.gif', images, duration = 500, loop = 0)
+iio.imwrite('wojakmeme.gif', images, duration = 500, loop = 0)
