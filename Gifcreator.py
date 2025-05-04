a7624209df90ba1bd5451c832d8cfdc145a6303a
Leon287 <dhruvjp04@gmail.com>
2025-05-04 10:41:57 +0530

    Added hippocorn images and created a gif

--- Gifcreator.py
+++ Gifcreator.py
@@ -1,9 +1,9 @@
 import imageio.v3 as iio 
 
-filenames = ['images\\image1.png','images\\image2.png','images\\image3.png']
+filenames = ['images\\hippocorn1.png','images\\hippocorn2.png','images\\hippocorn3.png','images\\hippocorn4.png']
 images = []
 
 for filename in filenames:
     images.append(iio.imread(filename))
 
-iio.imwrite('wojakmeme.gif', images, duration = 500, loop = 0)
+iio.imwrite('hippocorn.gif', images, duration = 500, loop = 0)
