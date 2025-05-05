bdb919f56e23e1bbcd7dbe8b88befed913d83128
Leon287 <dhruvjp04@gmail.com>
2025-05-05 23:23:27 +0530

    Update Wordle.py

--- Wordle/Wordle.py
+++ Wordle/Wordle.py
@@ -9,19 +9,22 @@ word_bank = [
 word = random.choice(word_bank)
 guessedWord = ['_'] * len(word)
 attempts = 10
-while attemps > 0:
-    print('\nCurrent word: '+' '.join(guessedWord))
-    guess = input('Guess a letter: ')
+while attempts > 0:
+   
+    print('\nCurrent word: ' + ' '.join(guessedWord))
+
+    guess = input('Guess a letter: ').lower()
+   
     if guess in word:
         for i in range(len(word)):
-            if word[i]==guess:
-                guessedWord[i] == guess
-        print('Great Guess!')
+            if word[i] == guess:
+                guessedWord[i] = guess
+        print('Great guess!')
     else:
-        attempts -=1
-        print(f"Wrong gess! Attempts left: {attempts}")
+        attempts -= 1
+        print('Wrong guess! Attempts left: ' + str(attempts))
     if '_' not in guessedWord:
-        print('\nCongratulations!! You guessed the word: ', word)
+        print('\nCongratulations!! You guessed the word: ' + word)
         break
-    else:
-        print('\nYou\'ve run out of attempts! The word was: ',word)
+else:
+      print('\nYou\'ve run out of attempts! The word was: ' + word)
