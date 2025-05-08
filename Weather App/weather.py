1cf24edcf92d86e8e554fcb12afe5420e2340cdb
Leon287 <dhruvjp04@gmail.com>
2025-05-08 19:21:34 +0530

    Added tempreature,humidity,description,feels like, sunset and sunrise time and wind speed variables

--- Weather App/weather.py
+++ Weather App/weather.py
@@ -10,11 +10,23 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = os.getenv('api_key')
 CITY = "London"
 
-# Build URL with correct formatting
-url = f"{BASE_URL}appid={API_KEY}&q={CITY}"
+def kelvin_to_celsiuse_fahrenheit(kelvin):
+    celsius = kelvin - 273.15
+    farhenheit = celsius * (9/5) + 32
+    return celsius, farhenheit
 
-# Make the request and print the response
+url = f"{BASE_URL}appid={API_KEY}&q={CITY}"
 response = requests.get(url).json()
 
-print(response)
+temp_kelvin = response['main']['temp']
+temp_celsius , temp_farenheit = kelvin_to_celsiuse_fahrenheit(temp_kelvin)
+
+feels_like_kelvin = response['main']['feels_like']
+feels_like_celsius, feels_like_fahrenheit = kelvin_to_celsiuse_fahrenheit(feels_like_kelvin)
+
+huimidity = response['main']['humidity']
+description = response['weather'][0]['description']
+sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
+sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+wind_speed = response['wind']['speed']
 
