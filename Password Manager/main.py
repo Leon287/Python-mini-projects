a1052f7c4fc76a5f282c10612d8c51bb0d714f42
Leon287 <dhruvjp04@gmail.com>
2025-05-09 19:30:53 +0530

    Added Comments
    Comments to describe what each function does

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,28 +1,28 @@
 from cryptography.fernet import Fernet
 
 class PasswordManager:
-    def __init__(self):
+    def __init__(self): # Initializes key, file path and dictionary to store passwords
         self.key = None
         self.password_file = None
         self.password_dict = {}
 
-    def create_key(self, path):
+    def create_key(self, path): # Generate a new encryption key and save it to given path
         self.key = Fernet.generate_key()
         with open(path,'wb') as f:
             f.write(self.key)
 
-    def load_key(self,path):
+    def load_key(self,path): # Load an existing key from a file
         with open(path,'rb') as f:
             self.key = f.read()
 
-    def create_password_file(self,path,initial_values=None):
+    def create_password_file(self,path,initial_values=None): # Make a new password file and add initial passwords
         self.password_file = path
 
         if initial_values is not None:
             for key, value in initial_values.items():
                 self.add_password(key,value)
     
-    def load_password_file(self,path):
+    def load_password_file(self,path): # Load existing passwords from a file
         self.password_file = path
 
         with open(path,'r') as f:
@@ -30,7 +30,7 @@ class PasswordManager:
                 site, encrypted =line.split(":")
                 self.password_dict[site] = Fernet(self.key).decrypt(encrypted.encode()).decode()
         
-    def add_password(self,site,password):
+    def add_password(self,site,password): # Add a new password for a site, encrypt it and save it to the first file
         self.password_dict[site] = password
 
         if self.password_file is not None:
@@ -38,7 +38,7 @@ class PasswordManager:
                 encrypted = Fernet(self.key).encrypt(password.encode())
                 f.write(site + ":" + encrypted.decode() + "\n")
     
-    def get_password(self,site):
+    def get_password(self,site): # Retrives decrypted password for a given site
         return self.password_dict[site]
     
 def main():
