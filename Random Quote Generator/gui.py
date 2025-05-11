b25b2dd1daa557f17c6a7ffe37a1b774d93fa88e
Leon287 <dhruvjp04@gmail.com>
2025-05-11 22:49:12 +0530

    changed fonts

--- Random Quote Generator/gui.py
+++ Random Quote Generator/gui.py
@@ -7,7 +7,7 @@ root.geometry("400x400")
 quote_text = tk.StringVar()
 quote_text.set("Click the button to get a quote!")
 
-quote_label = tk.Label(root, textvariable=quote_text, justify="center")
+quote_label = tk.Label(root, textvariable=quote_text, justify="center", font=("Comic Sans MS",12))
 quote_label.pack(pady=40)
 
 
