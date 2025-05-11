69e16bf08fd5ebdd281ca56f98dd8d213107b912
Leon287 <dhruvjp04@gmail.com>
2025-05-11 22:40:10 +0530

    Update quotegenerator.py

--- Random Quote Generator/quotegenerator.py
+++ Random Quote Generator/quotegenerator.py
@@ -1,6 +1,14 @@
 import requests
 
-response = requests.get("https://zenquotes.io/api/random")
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    data = response.json()
+    return (f"{data[0]['q']} - {data[0]['a']}")
 
-data = response.json()
-print(f"{data[0]['q']} - {data[0]['a']}")
+while True:
+    quote = get_quote()
+    print(f"\n{quote}\n")
+    again = input("Do you want another quote? (y/n) :")
+    if again != 'y':
+        print("Goodbye")
+        break
