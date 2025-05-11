6db965d5f6b7280277f3679a4fa92d4e2e539fdd
Leon287 <dhruvjp04@gmail.com>
2025-05-11 22:41:43 +0530

    Added exception handling

--- Random Quote Generator/quotegenerator.py
+++ Random Quote Generator/quotegenerator.py
@@ -1,14 +1,17 @@
 import requests
 
 def get_quote():
-    response = requests.get("https://zenquotes.io/api/random")
-    data = response.json()
-    return (f"{data[0]['q']} - {data[0]['a']}")
+    try:
+        response = requests.get("https://zenquotes.io/api/random")
+        data = response.json()
+        return (f"{data[0]['q']} - {data[0]['a']}")
+    except Exception as e:
+        return "Could not fetch a quote. Please try again later."
 
 while True:
     quote = get_quote()
     print(f"\n{quote}\n")
-    again = input("Do you want another quote? (y/n) :")
+    again = input("Do you want another quote? (y/n) :").strip().lower()
     if again != 'y':
         print("Goodbye")
         break
