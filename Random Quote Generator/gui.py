01008260e0833a1951241af8266752437e39bb81
Leon287 <dhruvjp04@gmail.com>
2025-05-11 22:46:59 +0530

    Update gui.py

--- Random Quote Generator/gui.py
+++ Random Quote Generator/gui.py
@@ -4,4 +4,11 @@ root = tk.Tk()
 root.title("Random Quote Generator")
 root.geometry("400x400")
 
+quote_text = tk.StringVar()
+quote_text.set("Click the button to get a quote!")
+
+quote_label = tk.Label(root, textvariable=quote_text, justify="center")
+quote_label.pack(pady=40)
+
+
 root.mainloop()
