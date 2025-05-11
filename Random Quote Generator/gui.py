4585eb49bf6224a8a05842e1630c74c60a90f2a2
Leon287 <dhruvjp04@gmail.com>
2025-05-11 22:54:10 +0530

    Update gui.py

--- Random Quote Generator/gui.py
+++ Random Quote Generator/gui.py
@@ -3,7 +3,6 @@ import requests
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
-    response.raise_for_status()
     data = response.json()
     quote = f'"{data[0]["q"]}" - {data[0]["a"]}'
     quote_text.set(quote)
@@ -15,7 +14,7 @@ root.geometry("400x400")
 quote_text = tk.StringVar()
 quote_text.set("Click the button to get a quote!")
 
-quote_label = tk.Label(root, textvariable=quote_text, justify="center", font=("Comic Sans MS",12))
+quote_label = tk.Label(root, textvariable=quote_text, justify="center", font=("Comic Sans MS",12),wraplength=350)
 quote_label.pack(pady=40)
 
 get_quote_button = tk.Button(root, text="Get Quote", command=get_quote(), font=("Comic Sans MS",12))
