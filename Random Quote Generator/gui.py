144fd81d0ad36c892ff88b0f6a9f0558626b3578
Leon287 <dhruvjp04@gmail.com>
2025-05-11 22:56:00 +0530

    Update gui.py

--- Random Quote Generator/gui.py
+++ Random Quote Generator/gui.py
@@ -2,9 +2,15 @@ import tkinter as tk
 import requests
 
 def get_quote():
-    response = requests.get("https://zenquotes.io/api/random")
-    data = response.json()
-    quote = f'"{data[0]["q"]}" - {data[0]["a"]}'
+    try:
+        response = requests.get("https://zenquotes.io/api/random")
+        data = response.json()
+        return (f"{data[0]['q']} - {data[0]['a']}")
+    except Exception as e:
+        return "Could not fetch a quote. Please try again later."
+    
+def show_quote():
+    quote = get_quote()
     quote_text.set(quote)
 
 root = tk.Tk()
@@ -17,7 +23,7 @@ quote_text.set("Click the button to get a quote!")
 quote_label = tk.Label(root, textvariable=quote_text, justify="center", font=("Comic Sans MS",12),wraplength=350)
 quote_label.pack(pady=40)
 
-get_quote_button = tk.Button(root, text="Get Quote", command=get_quote(), font=("Comic Sans MS",12))
+get_quote_button = tk.Button(root, text="Get Quote", command=show_quote(), font=("Comic Sans MS",12))
 get_quote_button.pack()
 
 
